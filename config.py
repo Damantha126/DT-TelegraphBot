8dc14c955b79c393388772156954d9a41ba031e8
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-16 07:25:27 +0530

    Telegraph Bot 📤

--- config.py
+++ config.py
@@ -7,9 +7,3 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
-	
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-	
-    DATABASE_URL = os.environ.get("DATABASE_URL")
