b7a7a2af4740b3731c84f820d7fef688cd8da355
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-16 07:24:55 +0530

    Telegraph Bot 📤

--- DTbot.py
+++ DTbot.py
@@ -1,22 +1,9 @@
 import os
 import logging
-import time
-import math
-import json
-import string
-import random
-import traceback
-import asyncio
-import datetime
-import aiofiles
-from random import choice
 from pyrogram import Client, filters
+from telegraph import upload_file
 from config import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from database import Database
-
-
-
 
 DTbot = Client(
    "Telegraph Uploader",
@@ -24,16 +11,6 @@ DTbot = Client(
    api_hash=Config.API_HASH,
    bot_token=Config.TG_BOT_TOKEN,
 )
-
-## --- Sub Configs --- ##
-BOT_USERNAME = Config.BOT_USERNAME
-TG_BOT_TOKEN = Config.TG_BOT_TOKEN
-APP_ID = Config.APP_ID
-API_HASH = Config.API_HASH
-BOT_OWNER = Config.BOT_OWNER
-db = Database(Config.DATABASE_URL, BOT_USERNAME)
-broadcast_ids = {}
-Bot = Client(BOT_USERNAME, bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH)
 
 @DTbot.on_message(filters.command("start"))
 async def start(client, message):
@@ -99,77 +76,6 @@ async def about(client, message):
                             ),        
             disable_web_page_preview=True,        
             parse_mode="html")
-
-@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
-async def broadcast_(c, m):
-	all_users = await db.get_all_users()
-	broadcast_msg = m.reply_to_message
-	while True:
-	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-	    if not broadcast_ids.get(broadcast_id):
-	        break
-	out = await m.reply_text(
-	    text = f"Broadcast Started! You will be notified with log file when all the users are notified."
-	)
-	start_time = time.time()
-	total_users = await db.total_users_count()
-	done = 0
-	failed = 0
-	success = 0
-	broadcast_ids[broadcast_id] = dict(
-	    total = total_users,
-	    current = done,
-	    failed = failed,
-	    success = success
-	)
-	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-	    async for user in all_users:
-	        sts, msg = await send_msg(
-	            user_id = int(user['id']),
-	            message = broadcast_msg
-	        )
-	        if msg is not None:
-	            await broadcast_log_file.write(msg)
-	        if sts == 200:
-	            success += 1
-	        else:
-	            failed += 1
-	        if sts == 400:
-	            await db.delete_user(user['id'])
-	        done += 1
-	        if broadcast_ids.get(broadcast_id) is None:
-	            break
-	        else:
-	            broadcast_ids[broadcast_id].update(
-	                dict(
-	                    current = done,
-	                    failed = failed,
-	                    success = success
-	                )
-	            )
-	if broadcast_ids.get(broadcast_id):
-	    broadcast_ids.pop(broadcast_id)
-	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-	await asyncio.sleep(3)
-	await out.delete()
-	if failed == 0:
-	    await m.reply_text(
-	        text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-	        quote=True
-	    )
-	else:
-	    await m.reply_document(
-	        document='broadcast.txt',
-	        caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-	        quote=True
-	    )
-	await os.remove('broadcast.txt')
-
-@Bot.on_message(filters.private & filters.command("stats") & filters.user(BOT_OWNER))
-async def sts(c, m):
-	total_users = await db.total_users_count()
-	await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
-
       
 @DTbot.on_message(filters.photo)
 async def telegraphphoto(client, message):
@@ -229,12 +135,6 @@ async def button(bot, update):
         await update.message.delete()
         await about(bot, update.message)
       elif "start" in cb_data:
-        await update.message.delete()
-        await start(bot, update.message)
-      elif "broadcast" in cb_data:
-        await update.message.delete()
-        await start(bot, update.message)
-      elif "stats" in cb_data:
         await update.message.delete()
         await start(bot, update.message)
 print(
